a9730595b44f94c70f05e9437037152770ea5b9e
Micheal Waltz <mwaltz@qualcomm.com>
2013-12-16 11:39:12 -0800

    Remove iso before re-creating it

--- buildiso.sh
+++ buildiso.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 #Build an ISO one directory up out of the current directory
 
-mkisofs -o ../steamoscustom.iso -b isolinux/isolinux.bin \
+outfile="../steamoscustom.iso"
+
+#Remove the ISO first if it was already created
+if [ -f ${outfile} ]; then
+	rm -fr ${outfile}
+fi
+
+mkisofs -o ${outfile} -b isolinux/isolinux.bin \
         -no-emul-boot -boot-load-size 4 \
         -boot-info-table -r .
