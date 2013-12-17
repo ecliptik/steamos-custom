0fcebda13b8881a595515eee7dbe6adb6fdf74a5
Micheal Waltz <ecliptik@gmail.com>
2013-12-16 20:04:37 -0800

    Use joliet and rockridge extensions, provide output

---
+++ buildiso.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#Build an ISO from one directory up
+
+outfile="steamoscustom.iso"
+
+#Remove the ISO first if it was already created
+if [ -f ${outfile} ]; then
+	rm -fr ${outfile}
+fi
+
+isodir=`pwd`
+	echo "Builingt ISO ${isodir}/${outfile}..."
+if genisoimage -o ${outfile} -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -RJ -r ..; then
+	echo "Built ISO ${isodir}/${outfile}"
+else
+	echo "Unable to build ISO ${isodir}/${outfile}"
+fi
